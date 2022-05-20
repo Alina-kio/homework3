51d90fcad06fd957e20dbbbca6afcf0decb133f1
Alina-kio <95016284+Alina-kio@users.noreply.github.com>
2022-05-20 19:09:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from aiogram.utils import executor
 from config import dp
 from hendlers import admin, client, callback, extra, fsm_anketa
 import logging
+from database import bot_db
 
+async def on_startup(_):
+    bot_db.sql_create()
+    
+    
 client.register_handler_client(dp)
 admin.register_handlers_admin(dp)
 fsm_anketa.register_hendler_fsmanketa(dp)
@@ -11,4 +16,4 @@ extra.register_hendler_extra(dp)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
